4ff6c5c844362f783a67c217704512da161d52f6
kimgs <kimgeunsukgs@gmail.com>
2021-07-17 22:39:32 +0900

    REINFORCE.py for discrete action

--- continuous/REINFORCE.py
+++ continuous/REINFORCE.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torch.distributions import Categorical
 from torch.utils.tensorboard import SummaryWriter
 
 import os
@@ -19,15 +20,13 @@ class PolicyNet(nn.Module):
         self.data = []
 
         self.fc1 = nn.Linear(num_states, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc_act = nn.Linear(64, num_actions)
+        self.fc2 = nn.Linear(128, num_actions)
         self.optimizer = optim.Adam(self.parameters(), lr=LEARNING_RATE)
 
     def forward(self, x):
         x = x.to(device)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = torch.tanh(self.fc_act(x))*2  # because the action space of the Pendulum-v0 is [-2,2]
+        x = F.softmax(self.fc2(x), dim=0)
         return x
 
     def put_data(self, item):
@@ -36,10 +35,10 @@ class PolicyNet(nn.Module):
     def train_net(self):
         R = 0
         self.optimizer.zero_grad()
-        for reward, action_prob in self.data[::-1]:
+        for reward, action_prob in self.data[::-1]: # repeat episode backwardly
             R = reward + GAMMA * R
             loss = -torch.log(action_prob) * R
-            loss.backward()
+            loss.backward()  # minimize loss = maximize policy
         self.optimizer.step()
         self.data = []
 
@@ -57,13 +56,12 @@ def disable_view_window():
 
 def main():
 
-    env = gym.make('Pendulum-v0')
+    env = gym.make('CartPole-v1')
     os.makedirs(f"./{COMMENT}", exist_ok=True)
     disable_view_window()
 
-    # store information about action and state dimensions
-    NUM_STATES = env.observation_space.shape[0]  # 3
-    NUM_ACTIONS = env.action_space.shape[0]      # 1
+    NUM_STATES = env.observation_space.shape[0]  # 4
+    NUM_ACTIONS = env.action_space.n  # 2
 
     policy_net = PolicyNet(NUM_STATES, NUM_ACTIONS).to(device)
     fig, ax = plt.subplots()
@@ -83,20 +81,20 @@ def main():
                 im = ax.imshow(env.render(mode='rgb_array'), animated=True)
                 ims.append([im])
 
-            action_prob = policy_net(torch.from_numpy(state).float())
-
-            next_state, reward, done, _ = env.step([action_prob.item()])
-            reward = reward / 100.0
+            action_prob = policy_net(torch.from_numpy(state).float())  # softmax output of discrete action
+            m = Categorical(action_prob)
+            action = m.sample()  # stochastic discrete action sampling
+            next_state, reward, done, _ = env.step(action.item())
 
             reward = torch.tensor([reward], device=device)
-            policy_net.put_data((reward, action_prob))
+            policy_net.put_data((reward, action_prob[action]))
 
             state = next_state
-            reward_sum += reward.item()
+            reward_sum += round(reward.item())
 
-            if done is True:  # Pendulum-v0 automatically terminated in 200th step.
+            if done is True:
                 if i_episode % 100 == 0:
-                    print(f"Return: {reward_sum:2f}")
+                    print(f"Return: {reward_sum}")
                     print()
                 writer.add_scalar("Return", reward_sum, i_episode)
                 policy_net.train_net()  # REINFORCE is update the policy at the end of the episode
@@ -104,7 +102,7 @@ def main():
 
         if i_episode > GIF_THRESH:
             ani = Animation.ArtistAnimation(fig, ims, interval=10)
-            ani.save(f'./{COMMENT}/episode_{i_episode}_return_{round(reward_sum)}.gif')
+            ani.save(f'./{COMMENT}/episode_{i_episode}_return_{reward_sum}.gif')
 
         env.close()
 
@@ -114,10 +112,14 @@ if __name__ == '__main__':
     # Hyperparameters
     GAMMA = 0.99
     LEARNING_RATE = 0.0005
-    NUM_EPS = 100_000
-    GIF_THRESH = 99_950
-    COMMENT = "REINFORCE_pendulum"
+    NUM_EPS = 10_000
+    GIF_THRESH = 9_990
+    COMMENT = "REINFORCE_cartpole"
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     main()
+'''
+note that torch.distributions page describe score function of REINFORCE algorithm.
+https://pytorch.org/docs/stable/distributions.html
+'''
